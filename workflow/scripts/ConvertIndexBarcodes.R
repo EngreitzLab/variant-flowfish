ff0d8e4a9bda8564dcdeb05e6a9e4ada5bff0f8f
Jesse Engreitz <engreitz@stanford.edu>
2021-05-31 12:28:37 -0700

    bug fix

--- workflow/scripts/ConvertIndexBarcodes.R
+++ workflow/scripts/ConvertIndexBarcodes.R
@@ -6,19 +6,23 @@ option.list <- list(
   make_option("--sampleList", type="character", help="Sample sheet with columns SampleID,Index1,Index2"),
   make_option("--i5", type="character", default="variant-flowfish/resources/2Pi5IndexedPrimersBarcodes.txt", help="File with map of i5 (Index1) labels to barcodes"),
   make_option("--i7", type="character", default="variant-flowfish/resources/2Pi7IndexedPrimersBarcodes.txt", help="File with map of i7 (Index2) labels to barcodes"),
-  make_option("--outfile", type="character", help="Output file"),
+  make_option("--outfile", type="character", default="", help="Output file. Default: stdout"),
   make_option("--append", type="logical", default=FALSE, help="Set TRUE to append output to --outfile instead of overwriting")
   )
 opt <- parse_args(OptionParser(option_list=option.list))
-dput(opt)
-
+#dput(opt)
 
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(tidyr))
 
 samplesheet <- read.delim(opt$sampleList, check.names=F, stringsAsFactors=F) %>% select(SampleID,Index1,Index2)
-i5 <- read.delim(opt$i5)
-i7 <- read.delim(opt$i7)
+
+if (any(duplicated(samplesheet$SampleID))) {
+	stop("Found duplicate SampleIDs:\n", paste0(with(samplesheet, SampleID[duplicated(SampleID)]), collapse="\n"))
+}
+
+i5 <- read.delim(opt$i5) %>% select(Index2,Index2Sequence)
+i7 <- read.delim(opt$i7) %>% select(Index1,Index1Sequence)
 res <- merge(samplesheet, i5, by="Index2", all.x=TRUE)
 res <- merge(res, i7, by="Index1", all.x=TRUE)
 res <- res %>% mutate(SampleID=ordered(SampleID, levels=samplesheet$SampleID)) %>% arrange(SampleID)
