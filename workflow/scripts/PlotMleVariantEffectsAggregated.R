1cb185493bc167c739029e77cf0a35fc3362d606
Katherine Guo <katherguo@gmail.com>
2022-03-10 11:51:15 -0800

    add dot plots for effect sizes by amplicon

--- workflow/scripts/PlotMleVariantEffectsAggregated.R
+++ workflow/scripts/PlotMleVariantEffectsAggregated.R
@@ -1,5 +1,5 @@
-# Jesse Engreitz
-# 12/15/21
+# Jesse Engreitz, Katherine Guo
+# 3/10/22
 # Rscript to plot MLE effect size estimates, combined across samples
 
 
@@ -16,11 +16,11 @@ dput(opt)
 
 if (FALSE) {
   ## For testing purposes
-  opt <- list()
-  #opt$variantInfo = "../../config/AlleleList.txt"
-  opt$outbase = "results/summary/DesiredVariants"
-  opt$samplesheet = "SampleList.snakemake.tsv"
-  opt$variantCounts = "results/summary/VariantCounts.DesiredVariants.flat.tsv"
+    opt <- list()
+    opt$allelicEffectFile = "results/summary/AllelicEffects.byPCRRep.ExperimentIDPCRRep.flat.tsv.gz"
+    opt$samplesheet = "SampleList.snakemake.tsv"
+    opt$outfile = "results/summary/AllelicEffects.byPCRRep.ExperimentIDPCRRep.pdf"
+    opt$effectColumn = "effect_size"
 }
 
 
@@ -37,93 +37,58 @@ suppressPackageStartupMessages(library(tidyr))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(cowplot))
-
+suppressPackageStartupMessages(library(Hmisc))
 
 mle <- read.delim(opt$allelicEffectFile, check.names=F, stringsAsFactors=F)
 stopifnot(opt$effectColumn %in% colnames(mle))
+mle <- subset(mle, sum1 >= 1000) ## Only plot the variants with at least 1000 cells
 
 samplesheet <- read.delim(opt$samplesheet, check.names=F, stringsAsFactors=F)
 
+pdf(file=opt$outfile, width=12, height=6)
 
-p1 <- ggplot(mle, aes_string(x="VariantID", y=opt$effectColumn)) +
-     geom_col() + 
-     ylab("Effect size on gene expression\n(MLE, vs reference allele)") +
-     geom_hline(yintercept=1, linetype="dashed", color="black") + 
-     theme_classic() +
-     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=6)) 
-
-p2 <- mle %>% mutate(PctEffect=(get(opt$effectColumn)-1)*100) %>%
-     ggplot(aes_string(x="VariantID", y="PctEffect")) +
-     geom_col() + 
-     ylab("Effect size on gene expression\n(MLE, % change vs reference allele)") +
-     geom_hline(yintercept=0, linetype="dashed", color="black") + 
-     theme_classic() +
-     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=6)) 
-
-pdf(file=opt$outfile, width=3, height=4)
-print(p1)
-print(p2)
-dev.off()
-
-
-
-
-
-
-############################################
-## Plot variant frequencies across bins
-
-getBinnedBarplot <- function(mle, samples, idcol) {
-
-  counts <- mle %>%
-            merge(samples %>% select_at(c(idcol,"AmpliconID")) %>% unique()) %>%
-            group_by(VariantID) %>%
-            mutate(GroupedFrequencyAvg=mean(freq),
-                   Variant=paste0(VariantID,"\n(",format(GroupedFrequencyAvg, digits=2, scientific=FALSE),"%)"),
-                   Replicate=factor(1:n()),
-                   PctEffect=(get(opt$effectColumn)-1)*100) %>%
-            as.data.frame()
-
-  p <- counts %>% 
-       ggplot(aes_string(x="Variant", y="PctEffect", fill="Replicate")) +
-       geom_bar(stat="identity", position=position_dodge()) +
-       scale_fill_grey(start=0.8, end=0.2) +
-       geom_hline(yintercept=0, linetype="dashed", color="black") +
-       ylab("Effect Size (% vs reference allele)") +
-       theme_classic() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=6)) +
-       theme(legend.key.size = unit(0.5, 'cm'), #change legend key size
-                 legend.title = element_text(size=9), #change legend title font size
-                 legend.text = element_text(size=8)) #change legend text font size
-
-  p2 <- counts %>%
-       ggplot(aes_string(x="Variant", y="PctEffect")) +
-       geom_boxplot(outlier.shape=NA, lwd=0.2) + geom_point(lwd=0.2, size=0.2) + 
-       scale_fill_grey(start=0.8, end=0.2) + scale_color_grey(start=0.8, end=0.2) +
-       geom_hline(yintercept=0, linetype="dashed", color="black") +
-       ylab("Effect Size (% vs reference allele)") +
-       theme_classic() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=6)) +
-       theme(legend.key.size = unit(0.5, 'cm'), #change legend key size
-                 legend.title = element_text(size=9), #change legend title font size
-                 legend.text = element_text(size=8)) #change legend text font size
-
-  q <- plot_grid(p, p2, nrow=2)
-  return(q)
-}
-
-
-idcol <- intersect(colnames(mle), colnames(samplesheet)) %>% setdiff("AmpliconID")
-
-pdf(file=paste0(opt$outfile), width=7, height=8)
-for (expt in unique(samplesheet$ExperimentID)) {
-  currSamples <- samplesheet %>% filter(ExperimentID == expt)
-
-  if (any(currSamples[,idcol] %in% mle[,idcol])) {
-    title <- ggdraw() + draw_label(paste0("ExperimentID == ",expt), fontface='bold')
-    p <- getBinnedBarplot(mle, currSamples, idcol)
-    print(plot_grid(title, p, ncol=1, rel_heights=c(0.1, 1)))
-  }
+for (amplicon in unique(samplesheet$AmpliconID)) {
+    p1 <- ggplot(mle %>% filter(AmpliconID==amplicon), aes_string("VariantID", opt$effectColumn)) +
+        geom_dotplot(binaxis='y', 
+                    stackdir='center', 
+                    dotsize = .3) + 
+        stat_summary(aes(VariantID, effect_size), fun.data = mean_cl_normal, geom="errorbar") + 
+        stat_summary(fun.y=mean, geom="point", shape=18,
+                        size=2, color="red") + 
+        ylab("Effect size on gene expression\n(MLE, vs reference allele)") +
+        ggtitle(paste(amplicon, "Variant Effects")) +
+        geom_hline(yintercept=1, linetype="dashed", color="red") + 
+        scale_y_continuous(limits=c(0, 1.5)) + 
+        theme_classic() +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=6), plot.title = 
+        element_text(hjust = 0.5)) +
+        theme(strip.background = element_blank(), strip.text.x = element_blank()) # don't show facet grid labels
+    if (length((mle %>% filter(AmpliconID==amplicon))$VariantID) > 10) {
+        p1 <- p1 + facet_grid(cols = vars(Location), scales = "free_x", switch = "x", space = "free_x")
+    }
+    print(p1)
+  
+    p2 <- mle %>% filter(AmpliconID==amplicon) %>% mutate(PctEffect=(get(opt$effectColumn)-1)*100) %>%
+        ggplot(aes_string(x="VariantID", y="PctEffect")) +
+        geom_dotplot(binaxis='y', 
+                    stackdir='center', 
+                    dotsize = .3) + 
+        stat_summary(aes(VariantID, PctEffect), fun.data = mean_cl_normal, geom="errorbar") + 
+        stat_summary(fun.y=mean, geom="point", shape=18,
+                        size=2, color="red") + 
+        ylab("Effect size on gene expression\n(MLE, % change vs reference allele)") +
+        ggtitle(paste(amplicon, "Variant Effect Percent Change")) +
+        geom_hline(yintercept=0, linetype="dashed", color="red") + 
+        # facet_grid(cols = vars(Location), scales = "free_x", switch = "x", space = "free_x") +
+        theme_classic() +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=6), plot.title = 
+        element_text(hjust = 0.5)) +
+        theme(strip.background = element_blank(), strip.text.x = element_blank()) # don't show facet grid labels
+    if (length((mle %>% filter(AmpliconID==amplicon))$VariantID) > 10) {
+        p2 <- p2 + facet_grid(cols = vars(Location), scales = "free_x", switch = "x", space = "free_x")
+    }
+    print(p2)
 }
 invisible(dev.off())
-
 
 save.image(file=paste0(opt$outfile,".RData"))
