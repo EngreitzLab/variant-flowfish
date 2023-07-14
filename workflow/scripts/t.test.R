db7e16ccd02854dd32405235c065cf2763cd9cfc
Harold Jones <jonesh@sh03-04n23.int>
2023-07-14 16:58:09 -0700

    Added frequency for the Stats file output and messed around with the plotting for the power plots

--- workflow/scripts/t.test.R
+++ workflow/scripts/t.test.R
@@ -18,7 +18,7 @@ option.list <- list(
   make_option("--allelicEffectFile", type="character", help="Allelic effect flat file"),
   make_option("--outfile", type="character", help="Output stats table"),
   make_option("--effectColumn", type="character", default="effect_size"),
-  make_option("--repPlots", type="character", help="Output file name for replicate plots")
+  # make_option("--repPlots", type="character", help="Output file name for replicate plots")
 )
 opt <- parse_args(OptionParser(option_list=option.list))
 
@@ -34,70 +34,74 @@ mle <- read.delim(opt$allelicEffectFile, check.names=F, stringsAsFactors=F)
 stopifnot(opt$effectColumn %in% colnames(mle))
 mle$Percent_Effect <- (mle$effect_size - 1)*100
 
+## Local run
+# setwd('/Volumes/groups/engreitz/Projects/VariantEditing/FF/20230531-S019_P151_PPIF_enh_VFF/results/summary/')
+# mle <- read.delim("AllelicEffects.byExperimentRep.ExperimentIDReplicates.flat.tsv.gz", check.names = FALSE, stringsAsFactors =F)
+# mle$Percent_Effect <- (mle$effect_size - 1)*100
 
-## Drop references and variants without observations below 500
+## Drop references and variants without observations below freq 0.000
 mle <- subset(mle, effect_size != 1 & logMean != 0 & sum1 > 500)
 
 ## Capture the highest observed value to set limits of graphs
 high <- max(abs(mle$Percent_Effect))
 
 ## Plot Replicates
-pdf(file=opt$repPlots, width=9, height=9)
-
-corData <- unique.data.frame(mle['VariantID'])
-
-for (rep in unique(mle$BioRep)) {
-  counter <- paste0("BioRep", rep)
-  rep <- mle[mle$BioRep == rep, c('VariantID', 'Percent_Effect')]
-  rep <- aggregate(Percent_Effect ~ ., mean, data = rep)
-  corData <- merge(corData, rep)
-  corData <- corData %>% rename(!!counter := Percent_Effect)
-  
-}
-
-p1 <- ggplot(corData, aes(x=BioRep1, y=BioRep2)) +
-  geom_point() +
-  scale_x_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
-  scale_y_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
-  coord_cartesian(xlim=c(-high-(high/10), high+(high/10)), ylim=c(-high-(high/10), high+(high/10))) +
-  geom_smooth(method = 'lm', se = FALSE, color = 'red', fullrange = TRUE) +
-  geom_abline(intercept = 0, slope = 1, colour='black') +
-  geom_hline(yintercept = 0, colour='seashell4') +
-  geom_vline(xintercept = 0, colour='seashell4') +
-  theme_minimal() +
-  stat_cor() +
-  xlab("BioRep 1 % Effect") +
-  ylab("BioRep 2 % Effect") + 
-  ggtitle(label = "BioReplicate Correlation")
-
-p2 <- ggMarginal(p1, type = 'density', color = 'seashell3', fill = adjustcolor( "seashell3", alpha.f = 0.2))
-print(p2)
-
-
-for (rep in unique(mle$BioRep)) {
-  FF1 <- mle[mle$BioRep == rep & mle$FFRep == 1, c('VariantID', 'Percent_Effect')]
-  FF2 <- mle[mle$BioRep == rep & mle$FFRep == 2, c('VariantID', 'Percent_Effect')]
-  corData <- merge(FF1, FF2, by = 'VariantID', all = TRUE)
-  corData[is.na(corData)] <- 1
-  p3 <- ggplot(corData, aes(x=Percent_Effect.x, y=Percent_Effect.y)) +
-    geom_point() +
-    stat_cor() +
-    scale_x_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
-    scale_y_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
-    coord_cartesian(xlim=c(-high-(high/10), high+(high/10)), ylim=c(-high-(high/10), high+(high/10))) +
-    geom_smooth(method = 'lm', se = FALSE, color = 'red', fullrange = TRUE) +
-    geom_abline(intercept = 0, slope = 1, colour='gray1') +
-    geom_hline(yintercept = 0, colour='seashell4') +
-    geom_vline(xintercept = 0, colour='seashell4') +
-    theme_minimal() +
-    xlab("FlowFISH Rep1 % Effect") +
-    ylab("FlowFISH Rep2 % Effect") + 
-    ggtitle(label = paste0("FFReplicate Correlation: BioRep", rep))
-
-  print(p3)
-}
-
-invisible(dev.off())
+# pdf(file=opt$repPlots, width=9, height=9)
+# 
+# corData <- unique.data.frame(mle['VariantID'])
+# 
+# for (rep in unique(mle$BioRep)) {
+#   counter <- paste0("BioRep", rep)
+#   rep <- mle[mle$BioRep == rep, c('VariantID', 'Percent_Effect')]
+#   rep <- aggregate(Percent_Effect ~ ., mean, data = rep)
+#   corData <- merge(corData, rep)
+#   corData <- corData %>% rename(!!counter := Percent_Effect)
+#   
+# }
+# 
+# p1 <- ggplot(corData, aes(x=BioRep1, y=BioRep2)) +
+#   geom_point() +
+#   scale_x_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
+#   scale_y_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
+#   coord_cartesian(xlim=c(-high-(high/10), high+(high/10)), ylim=c(-high-(high/10), high+(high/10))) +
+#   geom_smooth(method = 'lm', se = FALSE, color = 'red', fullrange = TRUE) +
+#   geom_abline(intercept = 0, slope = 1, colour='black') +
+#   geom_hline(yintercept = 0, colour='seashell4') +
+#   geom_vline(xintercept = 0, colour='seashell4') +
+#   theme_minimal() +
+#   stat_cor() +
+#   xlab("BioRep 1 % Effect") +
+#   ylab("BioRep 2 % Effect") + 
+#   ggtitle(label = "BioReplicate Correlation")
+# 
+# p2 <- ggMarginal(p1, type = 'density', color = 'seashell3', fill = adjustcolor( "seashell3", alpha.f = 0.2))
+# print(p2)
+# 
+# 
+# for (rep in unique(mle$BioRep)) {
+#   FF1 <- mle[mle$BioRep == rep & mle$FFRep == 1, c('VariantID', 'Percent_Effect')]
+#   FF2 <- mle[mle$BioRep == rep & mle$FFRep == 2, c('VariantID', 'Percent_Effect')]
+#   corData <- merge(FF1, FF2, by = 'VariantID', all = TRUE)
+#   corData[is.na(corData)] <- 1
+#   p3 <- ggplot(corData, aes(x=Percent_Effect.x, y=Percent_Effect.y)) +
+#     geom_point() +
+#     stat_cor() +
+#     scale_x_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
+#     scale_y_continuous(expand=c(0,0), limits=c(-high-(high/10), high+(high/10))) +
+#     coord_cartesian(xlim=c(-high-(high/10), high+(high/10)), ylim=c(-high-(high/10), high+(high/10))) +
+#     geom_smooth(method = 'lm', se = FALSE, color = 'red', fullrange = TRUE) +
+#     geom_abline(intercept = 0, slope = 1, colour='gray1') +
+#     geom_hline(yintercept = 0, colour='seashell4') +
+#     geom_vline(xintercept = 0, colour='seashell4') +
+#     theme_minimal() +
+#     xlab("FlowFISH Rep1 % Effect") +
+#     ylab("FlowFISH Rep2 % Effect") + 
+#     ggtitle(label = paste0("FFReplicate Correlation: BioRep", rep))
+# 
+#   print(p3)
+# }
+# 
+# invisible(dev.off())
 
 ## Compute T-test for every variant and mean effect size
 ## Build a new dataframe as an output
@@ -111,13 +115,14 @@ for (variant in unique(mle$VariantID)) {
   ## Select variants with at least 500 alleles per rep
   ## Choose the AmpliconID, total number of cells in rep, and the estimated effect size
   t.data <- mle[(mle$VariantID == variant & mle$sum1 > 500),]
-  t.data <- t.data[, c('AmpliconID', 'sum1', 'effect_size')]
+  t.data <- t.data[, c('AmpliconID', 'sum1', 'effect_size', 'freq')]
   ## Initialize values for power code
   ## Calculate statistics
   p.value <- 1
   MinReps <- NA
   Power <- NA
   SD <- sd(t.data$effect_size)
+  freq <- mean(t.data$freq)
   Mean_Effect <- mean(t.data$effect_size)
   mean.cellCount <- mean(t.data$sum1)
   sd.cellCount <- sd(t.data$sum1)
@@ -145,7 +150,7 @@ for (variant in unique(mle$VariantID)) {
       }
     }
   }
-  variantStats[variant, c('Mean_Effect', 'p.value', 'SD', 'MinReps', 'Power', 'mean.cellCount', 'sd.cellCount', 'total.cellCount')] <- c(Mean_Effect, p.value, SD, MinReps, Power, mean.cellCount, sd.cellCount, total.cellCount)
+  variantStats[variant, c('Mean_Effect', 'p.value', 'SD', 'MinReps', 'Power', 'mean.cellCount', 'sd.cellCount', 'freq', 'total.cellCount')] <- c(Mean_Effect, p.value, SD, MinReps, Power, mean.cellCount, sd.cellCount, freq, total.cellCount)
 }
 
 
@@ -156,3 +161,10 @@ variantStats["BH.p.value"] <- p.adjust(variantStats$p.value, "fdr", nrow(variant
 ## Write data
 write.table(variantStats, file = opt$outfile, sep='\t', col.names = NA)
 save.image(file=paste0(opt$outfile,".RData"))
+
+## Local
+## write.table(variantStats, file = "AllelicEffectsStats.tsv", sep='\t', col.names = NA)
+
+
+
+
